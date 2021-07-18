dc87c139fff70d9d2fb8cc065d0918ab6bbeccab
AdilsonAngelo <adilson.angelo.jr@gmail.com>
2021-07-18 10:44:22 -0300

    IMDb service: Implements fetch_episode and fetch_tvshow

--- lib/previously/imdb/imdb_helper.ex
+++ lib/previously/imdb/imdb_helper.ex
@@ -2,30 +2,65 @@ defmodule Previously.IMDb.IMDbHelper do
   @base_url "http://omdbapi.com/"
   @api_key System.get_env("OMDB_API_KEY")
 
-
   def search(%{"s" => _} = params) do
     params
     |> Map.put("type", "series")
     |> build_url()
-    |> HTTPoison.get!()
+    |> HTTPoison.get()
+    |> parse_response()
+  end
+
+  def fetch_episode(ep_code) do
+    %{i: ep_code, type: "episode"}
+    |> build_url()
+    |> HTTPoison.get()
+    |> parse_response()
+  end
+
+  def fetch_tvshow(tvshow_code) do
+    %{i: tvshow_code, type: "series"}
+    |> build_url()
+    |> HTTPoison.get()
     |> parse_response()
   end
 
   def get_episodes(imdb_id, season \\ 1) do
     %{i: imdb_id, season: season}
     |> build_url()
-    |> HTTPoison.get!()
-    |> parse_response
+    |> HTTPoison.get()
+    |> parse_response()
+  end
+
+  def imdb_string_to_date(date_string) do
+    regex_imdb = ~r/^(?<d>\d{2}) (?<m>\w+) (?<y>\d{4})$/
+
+    with %{"y" => year, "m" => month, "d" => day} <- Regex.named_captures(regex_imdb, date_string) do
+      Date.from_iso8601!("#{year}-#{month_map()[month]}-#{day}")
+    end
   end
 
   defp build_url(%{} = query_params) do
     query_params = Map.put(query_params, "apikey", @api_key)
 
     @base_url
-    |> URI.parse
+    |> URI.parse()
     |> Map.put(:query, URI.encode_query(query_params))
     |> URI.to_string()
   end
 
-  defp parse_response(%{body: body}), do: Jason.decode!(body)
+  defp parse_response({_, %{body: body}}) do
+    case Jason.decode(body) do
+      {:ok, result} -> result
+      {:error, _} -> nil
+    end
+  end
+
+  defp month_map do
+    ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    |> Enum.zip(1..12)
+    |> Enum.map(fn {k, v} ->
+      {k, String.pad_leading("#{v}", 2, "0")}
+    end)
+    |> Enum.into(%{})
+  end
 end
--- lib/previously/imdb/imdb_service.ex
+++ lib/previously/imdb/imdb_service.ex
@@ -1,5 +1,6 @@
 defmodule Previously.IMDb.IMDbService do
   alias Previously.IMDb.IMDbHelper
+  alias Previously.Episodes.Episode
 
   @spec search(String.t(), Integer.t()) :: any
   def search(query, page \\ 1) do
@@ -7,9 +8,56 @@ defmodule Previously.IMDb.IMDbService do
       %{"Search" => results, "Response" => "True", "totalResults" => total} ->
         total_num = String.to_integer(total)
         page = if page === nil, do: 1, else: page
-        {:ok, %{"results" => results, "total" => total_num, "page" => page }}
+        {:ok, %{"results" => results, "total" => total_num, "page" => page}}
+
       %{"Error" => err, "Response" => "False"} ->
         {:error, err}
+    end
+  end
+
+  def fetch_episode(ep_code) do
+    case IMDbHelper.fetch_episode(ep_code) do
+      %{
+        "Title" => title,
+        "Released" => release_date,
+        "Season" => season,
+        "Episode" => number,
+        "seriesID" => tvshow_imdb_id,
+        "imdbID" => ^ep_code,
+        "Type" => "episode"
+      } ->
+        episode = %{
+          imdb_id: ep_code,
+          season: String.to_integer(season),
+          number: String.to_integer(number),
+          release: IMDbHelper.imdb_string_to_date(release_date),
+          title: title
+        }
+
+        {:ok, episode, tvshow_imdb_id}
+
+      _ ->
+        {:error, nil, nil}
+    end
+  end
+
+  def fetch_tvshow(tvshow_code) do
+    case IMDbHelper.fetch_tvshow(tvshow_code) do
+      %{
+        "Title" => title,
+        "Poster" => poster,
+        "imdbID" => ^tvshow_code
+      } ->
+        tvshow = %{
+          imdb_id: tvshow_code,
+          poster: poster,
+          title: title
+        }
+
+        {:ok, tvshow}
+
+      _ ->
+        {:error, nil}
     end
   end
 end
